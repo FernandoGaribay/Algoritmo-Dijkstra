e7a10910cf3ea03dad57ebe3d9399ce766483d97
FernandoGaribay <garibaycejaf@gmail.com>
2023-04-02 00:45:46 -0600

    + Mapa.java -> 'mouseDragged'

--- src/main/Mapa.java
+++ src/main/Mapa.java
@@ -1,85 +1,61 @@
 package main;
 
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JButton;
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 public class Mapa extends JPanel {
 
-    private static final int numCasilla = 20;
-    private JButton casillas[][];
+    private static final int numCasilla = 30;
+    private JPanel casillas[][];
 
     public Mapa() {
-        casillas = new JButton[numCasilla][numCasilla];
+        casillas = new JPanel[numCasilla][numCasilla];
         crearMapa();
-
-//        this.addMouseListener(new MouseAdapter() {
-//            @Override
-//            public void mouseClicked(MouseEvent e) {
-//                int cellX = e.getX() / tamanoCasilla;
-//                int cellY = e.getY() / tamanoCasilla;
-//                {
-//                    System.out.println(e.getX() / tamanoCasilla);
-//                    System.out.println("Clicked on cell: " + cellX + ", " + cellY);
-//                }
-//            }
-//        });
-//        this.addMouseMotionListener(new MouseAdapter() {
-//            @Override
-//            public void mouseDragged(MouseEvent e) {
-//                int cellX = e.getX() / tamanoCasilla;
-//                int cellY = e.getY() / tamanoCasilla;
-//                {
-//                    System.out.println(e.getX() / tamanoCasilla);
-//                    System.out.println("Clicked on cell: " + cellX + ", " + cellY);
-//                }
-//
-//            }
-//        });
     }
 
-//    @Override
-//    public void paintComponent(Graphics g) {	//REPAINT
-//        super.paintComponent(g);
-//        for (int x = 0; x < numCasilla; x++) {	//PAINT EACH NODE IN THE GRID
-//            for (int y = 0; y < numCasilla; y++) {
-//                g.setColor(Color.WHITE);
-//                g.fillRect(x * tamanoCasilla, y * tamanoCasilla, tamanoCasilla, tamanoCasilla);
-//                g.setColor(Color.BLACK);
-//                g.drawRect(x * tamanoCasilla, y * tamanoCasilla, tamanoCasilla, tamanoCasilla);
-//            }
-//        }
-//    }
     private void crearMapa() {
         setLayout(new GridLayout(numCasilla, numCasilla));
 
         for (int fila = 0; fila < numCasilla; fila++) {
             for (int columna = 0; columna < numCasilla; columna++) {
-                casillas[fila][columna] = new JButton();
+                casillas[fila][columna] = new JPanel();
+                casillas[fila][columna].setBackground(Color.white);
+                casillas[fila][columna].setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
                 casillas[fila][columna].setName(fila + "," + columna);
                 add(casillas[fila][columna]);
 
-                casillas[fila][columna].addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mouseClicked(MouseEvent e) {
-                        btnClickIzquierdo((JButton) e.getSource());
-                    }
-                });
+                eventosBotones(fila, columna);
             }
         }
     }
-    
-    private void btnClickIzquierdo(JButton e) {
-        int coordenadas[] = obtenerCoordenadas(e);
-        casillas[coordenadas[0]][coordenadas[1]].setText("a");
-        System.out.println("coordenada: " + coordenadas[0] + "," + coordenadas[1]);
+
+    public void eventosBotones(int fila, int columna) {
+        casillas[fila][columna].addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                try {
+                    int ancho = ((JPanel) e.getSource()).getWidth();
+                    int x = (e.getX() + casillas[fila][columna].getX()) / ancho;
+                    int y = (e.getY() + casillas[fila][columna].getY()) / ancho;
+                    casillas[y][x].setBackground(Color.red);
+                } catch (Exception ex) {}
+            }
+        });
+
+        casillas[fila][columna].addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int coordenadas[] = obtenerCoordenadas((JPanel) e.getSource());
+                casillas[coordenadas[0]][coordenadas[1]].setBackground(Color.red);
+            }
+        });
     }
 
-    private int[] obtenerCoordenadas(JButton boton) {
+    private int[] obtenerCoordenadas(JPanel boton) {
         int coordenadas[] = new int[2];
         for (int i = 0; i < 2; i++) {
             coordenadas[i] = Integer.parseInt(boton.getName().split(",")[i]);
--- src/main/Nodo.java
+++ src/main/Nodo.java
@@ -2,16 +2,7 @@ package main;
 
 class Nodo {
 
-    // 0 = start, 1 = finish, 2 = wall, 3 = empty, 4 = checked, 5 = finalpath
-    private int cellType = 0;
-    private int hops;
-    private int x;
-    private int y;
+    public Nodo() {
 
-    public Nodo(int type, int x, int y) {
-        cellType = type;
-        this.x = x;
-        this.y = y;
-        hops = -1;
     }
 }
